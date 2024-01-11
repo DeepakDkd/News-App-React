1caecd9481e744a7b25beb1104aff25e02e0a943
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-01-12 00:22:34 +0530

    updated

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,111 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import SearchPage from './SearchPage'
+import React, { useEffect, useState } from 'react';
+import SearchPage from './SearchPage';
 
 function Homepage({ news }) {
-    const [bgTitle, setBgTitle] = useState('')
-    const [bgUrl, setBgUrl] = useState('')
-    
-    const [titles, setTitles] = useState([])
-    const [title, setTitle] = useState()
-    //    console.log(news && news.articles)
-    const intiSlide = "Welcome to Quick News, where we believe that staying informed shouldn't be a task but a delightful experience. Join us as we unfold the day's stories, painting a vivid picture with simple words and engaging narratives. Your journey with us promises not just news but a daily rendezvous with knowledge, made simple for you."
+    // State variables for background title and URL
+    const [bgTitle, setBgTitle] = useState('');
+    const [bgUrl, setBgUrl] = useState('');
 
+    // State variables for managing titles and current title
+    const [titles, setTitles] = useState([]);
+    const [title, setTitle] = useState();
+    const [headlineIndex , setHeadlineIndex] = useState(0)
+
+    // Initial slide text for the marquee
+    const intiSlide = "Welcome to Quick News, where we believe that staying informed shouldn't be a task but a delightful experience. Join us as we unfold the day's stories, painting a vivid picture with simple words and engaging narratives. Your journey with us promises not just news but a daily rendezvous with knowledge, made simple for you.";
+
+    // Background images for different categories
     const homebg = [
-        {
-            name: 'Politics',
-            url: 'https://cdn.narendramodi.in/wp-content/uploads/downloads/Wallpaper/8.jpg'
-        },
-        {
-            name: 'Science',
-            url: 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309_1280.jpg'
-        },
-        {
-            name: 'Health',
-            url: 'https://st.depositphotos.com/1907633/2382/i/600/depositphotos_23829545-stock-photo-medicine-doctor-hand-working-with.jpg'
-        },
-        {
-            name: 'Technology',
-            url: 'https://media.istockphoto.com/id/1209116250/photo/rapid-growth-concept-with-downtown-san-francisco-skyline.jpg?s=2048x2048&w=is&k=20&c=wyTzhXmyeICjy6vaZWCCeLy56sWs9fy24YC96c4ahio='
-        }, {
-            name: 'Entertainment',
-            url: 'https://media.istockphoto.com/id/104091659/photo/kuala-lumpur-malaysia.jpg?s=2048x2048&w=is&k=20&c=23LoG77yZKRaSvONxnzI83SDIf2zWmYzDgdSkZ7FRZ4='
-        },
-        {
-            name: 'Sports',
-            url: 'https://media.istockphoto.com/id/469569148/photo/soccer-fans-at-stadium.jpg?s=2048x2048&w=is&k=20&c=sewnfh2whhBzKN9eNxc3Qalz8DLXLUTGm-aJDPEot9s='
-        }
-    ]
+        { name: 'Politics', url: 'https://cdn.narendramodi.in/wp-content/uploads/downloads/Wallpaper/8.jpg' },
+        { name: 'Science', url: 'https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309_1280.jpg' },
+        { name: 'Health', url: 'https://st.depositphotos.com/1907633/2382/i/600/depositphotos_23829545-stock-photo-medicine-doctor-hand-working-with.jpg' },
+        { name: 'Technology', url: 'https://media.istockphoto.com/id/1209116250/photo/rapid-growth-concept-with-downtown-san-francisco-skyline.jpg?s=2048x2048&w=is&k=20&c=wyTzhXmyeICjy6vaZWCCeLy56sWs9fy24YC96c4ahio=' },
+        { name: 'Entertainment', url: 'https://media.istockphoto.com/id/104091659/photo/kuala-lumpur-malaysia.jpg?s=2048x2048&w=is&k=20&c=23LoG77yZKRaSvONxnzI83SDIf2zWmYzDgdSkZ7FRZ4=' },
+        { name: 'Sports', url: 'https://media.istockphoto.com/id/469569148/photo/soccer-fans-at-stadium.jpg?s=2048x2048&w=is&k=20&c=sewnfh2whhBzKN9eNxc3Qalz8DLXLUTGm-aJDPEot9s=' }
+    ];
 
+    // Change background image in intervals
     useEffect(() => {
-        let i = 0
+        let i = 0;
 
-        const Changer = setInterval(() => {
-            if (i < homebg.length) {
-                i = 0
+        const changerInterval = setInterval(() => {
+            if (i === homebg.length - 1) {
+                i = 0;
             }
-            setBgTitle(homebg[i].name)
-            setBgUrl(homebg[i].url)
+            setBgTitle(homebg[i].name);
+            setBgUrl(homebg[i].url);
 
-            i = i + 1
+            i += 1;
+        }, 4000);
 
-        }, 4000)
-        return () => clearInterval(Changer)
-    }, [])
+        // Cleanup interval on component unmount
+        return () => clearInterval(changerInterval);
+    }, []);
 
-
-
-    const titleArr = news && news.articles
-    console.log(titleArr)
-    // let ArrIdx = 0
-
+    // Set titles when news.articles change
     useEffect(() => {
+        setTitles(news.articles);
+    }, [news.articles]);
 
-        setTitles(news.articles)
-
-    }, [titleArr])
-
-    let titleIdx = 0
+    // Change current title in intervals
+    
     useEffect(() => {
-        const TitleTimer = setInterval(() => {
-            if (titleIdx < titles.length) {
-                titleIdx = 0
+        const titleTimer = setInterval(() => {
+            if (headlineIndex == titles.length - 1) {
+                setHeadlineIndex(0)
             }
-            // console.log(titles[0].title)
-            setTitle(titles[titleIdx])
-            titleIdx += 1
-        }, 25000)
+            setTitle(titles[headlineIndex]);
+            setHeadlineIndex(headlineIndex + 1);
+        }, 25000);
 
-        return () => clearInterval(TitleTimer)
-    }, [titles])
-
-
-
+        // Cleanup interval on component unmount
+        return () => clearInterval(titleTimer);
+    }, [headlineIndex]);
 
     return (
         <>
-
-
             <div className="home">
+                {/* Background image */}
                 <div className="pic" style={{ backgroundImage: `url(${bgUrl})` }}></div>
+                {/* Search bar */}
                 <SearchPage />
+                {/* Display current category */}
                 <div className="hometext">
-                    {/* <h1>Quick News </h1> */}
-                    {/* <h2>Stay Informed Anytime</h2> */}
                     <h2>{bgTitle}</h2>
-
                 </div>
-
+                {/* Display sliding news headlines */}
                 <div className='slidingNews'>
                     <div>Headlines</div>
-                    {
-                        title ? <marquee behavior="sliding" direction="left" key={title.title}>{title.title}</marquee> : <marquee behavior="sliding" direction="left" key={intiSlide}>{intiSlide}</marquee>
-
-                    }
+                    {/* Use marquee for sliding headlines */}
+                    {title ? <marquee behavior="sliding" direction="left" key={title.title}>{title.title}</marquee> : <marquee behavior="sliding" direction="left" key={intiSlide}>{intiSlide}</marquee>}
                 </div>
             </div>
-
         </>
-    )
+    );
 }
 
-export default Homepage
+export default Homepage;
--- src/Components/NewsField.jsx
+++ src/Components/NewsField.jsx
@@ -1,75 +1,105 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import useNews from '../Hooks/useNews';
 import Homepage from './Homepage';
 import { Link } from 'react-router-dom';
 import Loader from './Loader';
-function NewsField({ category, qry }) {
-    const [postSize, setPostSize] = useState(10)
-    const [page, setPage] = useState(1)
 
+function NewsField({ category, qry }) {
+    // State variables for managing pagination
+    const [postSize, setPostSize] = useState(10);
+    const [page, setPage] = useState(1);
+
+    // Default country for news API
     const country = "in";
+
+    // If category is not provided, set it to "general"
     if (!category) {
-        category = "general"
+        category = "general";
     }
 
+    // Fetch news data using custom hook
     const news = useNews({ country, category, postSize, qry });
 
-    console.log(news)
+    // Calculate total number of pages based on the number of articles
+    const totalPage = news ? Math.ceil(news.totalResults / 10) : 0;
 
-    const totalPage = news ? (Math.ceil(news.totalResults / 10)) : 0;
-
+    // Scroll to the top of the page when category or query changes
     useEffect(() => {
         setPage(1);
         setPostSize(10);
-        window.scrollTo({ top: 0, behavior: 'smooth' })
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, [category, qry]);
 
-    }, [category, qry])
-
+    // Function to load more news articles
     const getmore = () => {
-        setPage((prev) => prev + 1)
+        setPage((prev) => prev + 1);
+        setPostSize(postSize + 10);
+    };
 
-        setPostSize(postSize + 10)
-
-    }
     return news ? (
         <div className='field'>
-            {news && category == "general" ? <Homepage news={news && news} /> : null}
+            {/* Render Homepage component if the category is "general" */}
+            {news && category === "general" ? <Homepage news={news} /> : null}
 
-            {!news.totalResults == 0 ? (<>
-                <div className='PostField'>
-                    {
+            {/* Check if there are news articles */}
+            {!news.totalResults == 0 ? (
+                <>
+                    {/* Render news articles */}
+                    <div className='PostField'>
+                        {news &&
+                            news.articles.map((data) => (
+                                <div key={data.title} className='card'>
+                                    <h5>{data.source.name}</h5>
 
-                        news && news.articles.map((data) => (
+                                    <img
+                                        src={
+                                            data.urlToImage
+                                                ? data.urlToImage
+                                                : 'https://media.istockphoto.com/id/1397043853/video/digital-spinning-globe-animation-for-use-in-playback-videos-news-reports-loading-screens-3d.jpg?s=640x640&k=20&c=81N3o45ZRxKDmp1VLL1C9GnUjTmDRga_86iGAoX4BLU='
+                                        }
+                                        alt=''
+                                    />
 
-                            <div key={data.title} className="card">
-                                <h5>{data.source.name}</h5>
+                                    <h3>
+                                        <Link to={data.url} target='_blank'>
+                                            {data.title}
+                                        </Link>
+                                    </h3>
+                                    <p className='description'>{data.description}</p>
+                                    <p>{data.publishedAt.slice(0, 10)}</p>
+                                    <button onClick={() => window.open(`${data.url}`, '_blank')}>Read more</button>
+                                </div>
+                            ))}
+                    </div>
 
-                                <img src={data.urlToImage ? data.urlToImage : "https://media.istockphoto.com/id/1397043853/video/digital-spinning-globe-animation-for-use-in-playback-videos-news-reports-loading-screens-3d.jpg?s=640x640&k=20&c=81N3o45ZRxKDmp1VLL1C9GnUjTmDRga_86iGAoX4BLU="} alt="" />
-
-                                <h3>
-                                    <Link to={news && data.url} target='_blank'>{data.title}
-                                    </Link></h3>
-                                <p className='description'>{data.description}</p>
-                                <p>{data.publishedAt.slice(0, 10)}</p>
-                                <button onClick={() => window.open(`${data.url}`, '_blank')}>Read more</button>
-                            </div>
-                        ))
-                    }
-                </div >
-                <div>
-                    {news && page === totalPage ? (<span className='newsEnd'>You are up-to-date! Explore other categories for more news.</span>) : (<button className='nextBtn' onClick={getmore}>Get More</button>)}
-                </div> </>) : (<div className='noData'>
-                    <h1>Oops! No news found for <u>{qry}</u></h1>
+                    {/* Render pagination button or end message */}
+                    <div>
+                        {news && page === totalPage ? (
+                            <span className='newsEnd'>You are up-to-date! Explore other categories for more news.</span>
+                        ) : (
+                            <button className='nextBtn' onClick={getmore}>
+                                Get More
+                            </button>
+                        )}
+                    </div>
+                </>
+            ) : (
+                // Render no data message if no articles found
+                <div className='noData'>
+                    <h1>
+                        Oops! No news found for <u>{qry}</u>
+                    </h1>
                     <p>It seems like there are no news articles matching your search.</p>
                     <div className='noDataimg'>
-                        <img src="https://2.bp.blogspot.com/-SXNnmaKWILg/XoNVoMTrxgI/AAAAAAAAxnM/7TFptA1OMC8uk67JsG5PcwO_8fAuQTzkQCLcBGAsYHQ/s1600/giphy.gif" alt="No data found" />
+                        <img src='https://2.bp.blogspot.com/-SXNnmaKWILg/XoNVoMTrxgI/AAAAAAAAxnM/7TFptA1OMC8uk67JsG5PcwO_8fAuQTzkQCLcBGAsYHQ/s1600/giphy.gif' alt='No data found' />
                     </div>
                 </div>
-            )
-            }
-
+            )}
         </div>
-    ) : (<Loader />)
+    ) : (
+        // Render loader while fetching news data
+        <Loader />
+    );
 }
 
-export default NewsField
+export default NewsField;
